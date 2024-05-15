4cb1a43d49e7dc6c826f5a554c617a47ad80a198
TCordova21 <124856715+TCordova21@users.noreply.github.com>
2024-05-15 14:47:14 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProyecto.Entidades
+{
+    public class Proyecto
+    {
+        [Key]
+        public int Id { get; set; }   // PK 
+
+        [DisplayName("Nombre del Proyecto")]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        [DisplayName("Fecha de Inicio")]
+        public DateTime FechaInicio { get; set; }
+
+        [DisplayName("Fecha de Fin")]
+        public DateTime FechaFin { get; set; }
+
+        [DisplayName("Estado del Proyecto")]
+        public string Estado { get; set; }
+
+        public List<Tarea>? Tareas { get; set; }
+    }
+}
---
+++ Tarea.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppProyecto.Entidades
+{
+    public class Tarea
+    {
+        [Key]
+        public int Id { get; set; }   // PK 
+
+        [DisplayName("Descripción de la Tarea")]
+        public string Descripcion { get; set; }
+
+        [DisplayName("Fecha de Creación")]
+        public DateTime FechaCreacion { get; set; }
+
+        [DisplayName("Fecha de Vencimiento")]
+        public DateTime FechaVencimiento { get; set; }
+
+        [DisplayName("Estado de la Tarea")]
+        public string Estado { get; set; }
+
+        public int ProyectoId { get; set; }   // FK 
+
+        public Proyecto? Proyecto { get; set; }   // Relación de navegación
+    }
+}
